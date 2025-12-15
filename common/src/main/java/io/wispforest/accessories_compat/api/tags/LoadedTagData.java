b4e2c5431edb1415b86e8f9cdc23259993271a92
Dragon-Seeker <timekeeperguild@gmail.com>
2025-12-15 17:38:22 -0600

    Resolve issue with tag mutation API using immutable lists instead of ArrayList

--- common/src/main/java/io/wispforest/accessories_compat/api/tags/LoadedTagData.java
+++ common/src/main/java/io/wispforest/accessories_compat/api/tags/LoadedTagData.java
@@ -3,10 +3,11 @@ package io.wispforest.accessories_compat.api.tags;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagLoader;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public record LoadedTagData(ResourceLocation tagKey, List<TagLoader.EntryWithSource> entries) {
     public LoadedTagData(ResourceLocation tagKey) {
-        this(tagKey, List.of());
+        this(tagKey, new ArrayList<>());
     }
 }
